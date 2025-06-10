0e2decad5ee84cc4967d61631c288db2bec8d529
fbmcarmo <fbrunomcarmo@gmail.com>
2025-06-10 10:41:13 -0300

    pagina de produtos com get

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,6 +1,19 @@
 import PageWrapper from "@/components/PageWrapper";
+import instance from "@/instance/api";
+import { useEffect, useState } from "react";
 
 export default function Produtos(){
+    const [produtos, setProdutos] = useState([])
+
+    useEffect(() => {
+        async function getProdutos(){
+            const response = await instance.get('/produtos')
+            setProdutos(response.data)
+        }
+
+        getProdutos()
+    }, [])
+
     return (
         <PageWrapper>
             <p>Olá Mundo</p>
